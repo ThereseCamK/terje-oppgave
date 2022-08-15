85910e10fa210cf1875a84821d6ee07e4a7ffa29
Therese C Kjær <69575527+ThereseCamK@users.noreply.github.com>
2022-08-15 19:31:44 +0200

    knapper istedetfor links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
-import {Link } from 'react-router-dom';
+// import {Link } from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
 
 
 function App() {
+  // const [navigate, showChange] = useState('/Click')
+  const navigate = useNavigate();
+  function showClick(){
+    navigate('/Click')
+  }
+  function showClicker(){
+    navigate('/Clicker')
+  }
+  function showToDo(){
+    navigate('/toDoMain')
+  }
  
   return (
     <div className="App">
       <header className="App-header">
           <div>Forside</div>
-        <Link to="/Click" >til klikkergame uten hooks</Link>
+          <button onClick={showClick}>til klikker(uten hooks)</button>
+          <button onClick={showClicker}>til klikker (med hooks)</button>
+          <button onClick={showToDo}>til TODO</button>
+          {/* <button onClick={() =>showClicker(navigate)}>Til Klikk(uten hooks)</button> */}
+        {/* <Link to="/Click" >til klikkergame uten hooks</Link>
         <Link to="/Clicker" >til klikkergame med hooks</Link>
-        <Link to="/toDoMain" >til todDoList</Link>
+        <Link to="/toDoMain" >til todDoList</Link> */}
+
 
       </header>
     </div>
