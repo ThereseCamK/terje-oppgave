2d76484ef9b54b05c8a9df74493fd69b2cd3f5de
Therese C Kjær <69575527+ThereseCamK@users.noreply.github.com>
2022-08-16 08:50:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ import {useNavigate} from 'react-router-dom';
 function App() {
   // const [navigate, showClicker] = useState('/Click')
   const navigate = useNavigate();
-  function showClick(){
-    navigate('/Click')
-  }
-  function showClicker(){
-    navigate('/Clicker')
-  }
-  function showToDo(){
-    navigate('/toDoMain')
-  }
+  // function showClick(){
+  //   navigate('/Click')
+  // }
+  // function showClicker(){
+  //   navigate('/Clicker')
+  // }
+  // function showToDo(){
+  //   navigate('/toDoMain')
+  // }
  
   return (
     <div className="App">
       <header className="App-header">
           <div>Forside</div>
-          <button onClick={showClick}>til klikker(uten hooks)</button>
-          <button onClick={showClicker}>til klikker (med hooks)</button>
-          <button onClick={showToDo}>til TODO</button>
+          <button onClick={()=>navigate('/Click')}>til klikker(uten hooks)</button>
+          <button onClick={()=>navigate('/Clicker')}>til klikker (med hooks)</button>
+          <button onClick={()=>navigate('/toDoMain')}>til TODO</button>
           {/* <button onClick={() =>showClicker(navigate)}>Til Klikk(uten hooks)</button> */}
         {/* <Link to="/Click" >til klikkergame uten hooks</Link>
         <Link to="/Clicker" >til klikkergame med hooks</Link>
